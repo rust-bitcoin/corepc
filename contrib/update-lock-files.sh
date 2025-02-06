b657288f44a7168b9a7e6e104c0ffe9a88e25026
Tobin C. Harding <me@tobin.cc>
2025-02-06 13:56:15 +1100

    Run node tests
    Currently we have a job for each supported version of Bitcoin Core and
    in it we run the integration tests.
    
    Note also that `node` is currently not a member of the workspace. So it
    misses out on all the benefits of `run_task` (e.g., linting).
    
    However `node` cannot be directly run using `run_task` because of its
    unusual features - it requires a version feature to build.
    
    Add a script to lint and one to run tests then call these from CI.
    
    While we are at it update the justfile now that `node` can be linted and
    formatted as part of workspace commands. Also update the
    `update-lock-files` script.
    
    This PR adds a ton of dependencies to the lock file. Run `cargo` with
    `-Z minimal-versions` to try and get a minimal lock file that works.

--- contrib/update-lock-files.sh
+++ contrib/update-lock-files.sh
@@ -4,8 +4,17 @@
 
 set -euo pipefail
 
+REPO_DIR=$(git rev-parse --show-toplevel)
+
 for file in Cargo-minimal.lock Cargo-recent.lock; do
     cp --force "$file" Cargo.lock
-    cargo check
+
+    # Same crate list as in members section of workspace manifest.
+    for crate in "client" "types" "jsonrpc" "node"; do
+        pushd "$REPO_DIR/$crate" > /dev/null
+        cargo build --all-features
+        popd > /dev/null
+    done
+
     cp --force Cargo.lock "$file"
 done
